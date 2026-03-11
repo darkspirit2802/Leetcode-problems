a931f1d093feb712d9c53b0692ba4aa0930a083e
Vivek Tyagi <tyagivivek435@gmail.com>
2026-03-11 12:10:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        int counter = 0;
-        int result = 0;
-        if(n==0){
-            return 1;
+        int mask = 1;
+        while(mask < n){
+            mask = (mask<<1)|1;
         }
-        while(n){
-            int r = n%2;
-            result += (pow(2,counter) * (!r));
-            counter++;
-            n = n/2;
-        }
-        return result;
+
+        return mask ^ n;
     }
 };
