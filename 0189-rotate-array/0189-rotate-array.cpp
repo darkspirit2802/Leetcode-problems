0aec373d912046e9b479438c3d0a1ccd9bd2b891
Vivek Tyagi <tyagivivek435@gmail.com>
2026-05-16 02:11:13 +0530

    Time: 4 ms (4%), Space: 30.9 MB (5.26%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+
         int n = nums.size();
-        k = k%n;
-        std::rotate(nums.begin(),nums.end()-k,nums.end());
+        k = k % n;
+
+        vector<int> vec(2 * n);
+
+        // duplicate array
+        for(int i = 0; i < n; i++) {
+            vec[i] = nums[i];
+            vec[i + n] = nums[i];
+        }
+
+        // copy rotated part back to nums
+        for(int i = 0; i < n; i++) {
+            nums[i] = vec[n - k + i];
+        }
     }
 };
