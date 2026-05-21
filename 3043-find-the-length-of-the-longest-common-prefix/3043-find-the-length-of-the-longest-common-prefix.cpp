10041987f5eaed7d02253cc01069aa7902669d44
Vivek Tyagi <tyagivivek435@gmail.com>
2026-05-21 10:01:57 +0530

    Time: 121 ms (92.5%), Space: 134.7 MB (95.5%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<int>st;
+
+        for(int val : arr1){
+            while(!st.count(val) && val > 0){
+                st.insert(val);
+                val = val / 10;
+            }
+        }
+
+        int result  = 0;
+
+        for(int num : arr2){
+            while(num > 0 && !st.count(num)){
+                num = num/10;
+            }
+            if(num > 0){
+                result = max(result , static_cast<int>(log10(num) + 1));
+            }
+        }
+        return result;
+    }
+};
