83ad84f1d50a38dfb706a0deae93320d46ee5d5e
Vivek Tyagi <tyagivivek435@gmail.com>
2026-06-25 16:23:17 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.32%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool consecutiveSetBits(int n) {
+        int cnt = 0;
+
+        while (n > 0) {
+            if ((n & 1) && ((n >> 1) & 1))
+                cnt++;
+
+            if (cnt > 1) return false;
+            n >>= 1;
+        }
+
+        return cnt == 1;
+    }
+};
