bf32fd33f7e8c5a2e3ce11f5d0aaeaf793261220
Vivek Tyagi <tyagivivek435@gmail.com>
2026-07-01 13:38:00 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        for(int i = 0; i<n ;i++){
+            char ch = s[i];
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+               else {
+            if (st.empty()) return false;
+
+            char top = st.top();
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                return false;
+            }
+            st.pop();
+        }
+
+        }
+      
+        return st.empty();
+        
+    }
+};
